ada93aa11b7ad9fc590a97c5be63a54cf6f97b55
Yuzo <yoozoo.mng@gmail.com>
2019-12-23 14:09:11 +0900

    機能追加: windowサイズを取得

--- src/Main.tsx
+++ src/Main.tsx
@@ -1,3 +1,22 @@
 import * as React from 'react';
+import Field from './Field';
 
-export default () => <div className="main" />;
+export default function Main() {
+  const [size, setSize] = React.useState<{ width: number; height: number }>({
+    width: window.innerWidth,
+    height: window.innerHeight
+  });
+  React.useEffect(() => {
+    const onresize = (e: UIEvent) => {
+      setSize({ width: window.innerWidth, height: window.innerHeight });
+    };
+    window.addEventListener('resize', onresize);
+    return () => window.removeEventListener('resize', onresize);
+  }, []);
+
+  return (
+    <div className="main" style={size}>
+      <Field size={size} />
+    </div>
+  );
+}
